e159196d6739131a3c86a3beaf82bb5d8baef2a8
SyahrulApr86 <apriansyah.syahrul@gmail.com>
2024-11-25 14:06:39 +0700

    first commit

---
+++ install-driver-nvidia.sh
@@ -0,0 +1,8 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update -y
+sudo apt install ubuntu-drivers-common -y
+
+# sudo apt install nvidia-driver-<version> -y
+# sudo apt install nvidia-driver-560 -y
+
+sudo reboot
