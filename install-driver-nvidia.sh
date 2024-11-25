20c88c994116fa6b1ce9814a07fbf2833a031389
SyahrulApr86 <94279620+SyahrulApr86@users.noreply.github.com>
2024-11-25 14:21:36 +0700

    Update install-driver-nvidia.sh

--- install-driver-nvidia.sh
+++ install-driver-nvidia.sh
@@ -6,3 +6,11 @@ sudo apt install ubuntu-drivers-common -y
 # sudo apt install nvidia-driver-560 -y
 
 sudo reboot
+
+####################
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update -y
+sudo apt-get install -y nvidia-container-toolkit -y
